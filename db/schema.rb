1f3d3b87c154267204b9a38550437bce04b13913
Zack Forbing <zackforbing@gmail.com>
2017-03-04 14:03:03 -0700

    create Artwork table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304205957) do
+ActiveRecord::Schema.define(version: 20170304210215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artworks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "artist"
+    t.string   "description"
+    t.decimal  "map_lat"
+    t.decimal  "map_long"
+    t.string   "address"
+    t.integer  "user_id"
+    t.integer  "neighborhood_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["neighborhood_id"], name: "index_artworks_on_neighborhood_id", using: :btree
+    t.index ["user_id"], name: "index_artworks_on_user_id", using: :btree
+  end
 
   create_table "neighborhoods", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +61,8 @@ ActiveRecord::Schema.define(version: 20170304205957) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "artworks", "neighborhoods"
+  add_foreign_key "artworks", "users"
   add_foreign_key "stories", "neighborhoods"
   add_foreign_key "stories", "users"
 end
