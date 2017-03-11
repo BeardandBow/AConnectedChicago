a9eb31da5c00ea9c41c62800af78a25375cea756
Zack Forbing <zackforbing@gmail.com>
2017-03-11 16:10:50 -0700

    tested logout functionality (#32)

---
+++ spec/features/registered_user/user_can_log_in_and_log_out_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.feature "user can log in and log out" do
+  context "user logs in" do
+    scenario "user logs into their account" do
+      #  As a user
+      user = create(:user)
+      #  When I visit the home page
+      visit root_path
+      #  and click the login link
+      click_link "Login"
+      #  I should be able to enter my credentials and login
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "opensesame"
+      click_button "Login"
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_link("Logout")
+    end
+
+    scenario "user enters non-existent email" do
+      visit root_path
+      click_link "Login"
+
+      fill_in "Email", with: "anotherguy@gmail.com"
+      fill_in "Password", with: "opensesame"
+      click_button "Login"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("There is no user with that email")
+    end
+
+    scenario "user enters wrong password" do
+      user = create(:user)
+      visit root_path
+      click_link "Login"
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "closesesame"
+      click_button "Login"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Password is incorrect")
+    end
+  end
+
+  scenario "user logs out" do
+    user = create(:user)
+    visit root_path
+    click_link "Login"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "opensesame"
+
+    click_button "Login"
+    click_on "Logout"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_link("Login")
+    expect(page).not_to have_link("Logout")
+  end
+end
--- spec/features/visitor/visitor_can_create_account_spec.rb
+++ spec/features/visitor/visitor_can_create_account_spec.rb
@@ -10,13 +10,13 @@ RSpec.feature "visitor can create account" do
     visit root_path
     # I should be able to click the sign up link
     expect(page).to have_link("Create Account")
-    click_on "Create Account"
+    click_link "Create Account"
     # and create an account with credentials
     fill_in "Email", with: "someguy@gmail.com"
     fill_in "Password", with: "opensesame"
     fill_in "Password Confirmation", with: "opensesame"
     fill_in "Your Neighborhood", with: @neighborhood.name
-    click_on "Create Account"
+    click_button "Create Account"
 
     user = User.first
 
@@ -28,13 +28,13 @@ RSpec.feature "visitor can create account" do
 
   scenario "visitor enters bad password confirmation" do
     visit root_path
-    click_on "Create Account"
+    click_link "Create Account"
 
     fill_in "Email", with: "someguy@gmail.com"
     fill_in "Password", with: "opensesame"
     fill_in "Password Confirmation", with: "closesesame"
     fill_in "Your Neighborhood", with: @neighborhood.name
-    click_on "Create Account"
+    click_button "Create Account"
 
     expect(User.all.count).to eq(0)
     expect(page).to have_content("Password confirmation doesn't match password")
@@ -42,13 +42,13 @@ RSpec.feature "visitor can create account" do
 
   scenario "visitor does not enter password" do
     visit root_path
-    click_on "Create Account"
+    click_link "Create Account"
 
     fill_in "Email", with: "someguy@gmail.com"
     fill_in "Password", with: ""
     fill_in "Password Confirmation", with: "closesesame"
     fill_in "Your Neighborhood", with: @neighborhood.name
-    click_on "Create Account"
+    click_button "Create Account"
 
     expect(User.all.count).to eq(0)
     expect(page).to have_content("Password can't be blank")
