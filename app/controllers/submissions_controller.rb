d005368f6892003ab25d8f3beebf60ba16f2ef87
Caleb <calebcowen@gmail.com>
2017-03-14 17:40:42 -0600

    Fixed community leader submissions query

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -24,27 +24,33 @@ class SubmissionsController < ApplicationController
     submissions = []
     events = Event.where(status: "pending", organization: current_user.organizations)
     submissions << events if events
-    artworks = Artwork.where(status: "pending")
-    if artworks
-      submissions << artworks.find_all do |artwork|
-        current_user.organizations.include(artwork.organization) if artwork.organization
-      end
-      submissions << artworks.find_all do |artwork|
-        current_user.neighborhood == artwork.neighborhood && !artwork.organization
-      end
-    end
-    stories = Story.where(status: "pending")
-    if stories
-      submissions << stories.find_all do |story|
-        current_user.organizations.include(story.organization) if story.organization
-      end
-      submissions << stories.find_all do |story|
-        current_user.neighborhood == story.neighborhood && !story.organization
-      end
-    end
+    # artworks = Artwork.where(status: "pending")
+    # if artworks
+      # submissions << artworks.find_all do |artwork|
+        # current_user.organizations.include(artwork.organization) if artwork.organization
+      # end
+      submissions << Artwork.joins(:organization).where(organization_id: current_user.organizations, status: "pending")
+      submissions << Artwork.joins(:neighborhood).where(neighborhood_id: current_user.neighborhood, status: "pending")
+
+      submissions << Story.joins(:organization).where(organization_id: current_user.organizations, status: "pending")
+      submissions << Story.joins(:neighborhood).where(neighborhood_id: current_user.neighborhood, status: "pending")
+
+      # submissions << artworks.find_all do |artwork|
+      #   current_user.neighborhood == artwork.neighborhood && !artwork.organization
+      # end
+    # end
+    # stories = Story.where(status: "pending")
+    # if stories
+    #   submissions << stories.find_all do |story|
+    #     current_user.organizations.include(story.organization) if story.organization
+    #   end
+    #   submissions << stories.find_all do |story|
+    #     current_user.neighborhood == story.neighborhood && !story.organization
+    #   end
+    # end
 
     unless submissions.empty?
-      submissions.flatten.sort_by do |submission|
+      submissions.flatten.uniq.sort_by do |submission|
         submission.created_at if submission
       end
     end
