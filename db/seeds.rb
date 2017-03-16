61e8c047e5a33e2743e1e5bf201019aedc85f3f4
Zack Forbing <zackforbing@gmail.com>
2017-03-16 13:30:53 -0600

    seedfile updated with correct neighborhoods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,50 @@
 class Seed
   def self.start
-    hood_1 = Neighborhood.create(name: "Neighborhood 1")
-    hood_2 = Neighborhood.create(name: "Neighborhood 2")
-    hood_3 = Neighborhood.create(name: "Neighborhood 3")
-    hood_4 = Neighborhood.create(name: "Neighborhood 4")
+    neighborhood_names = ["Rogers Park", "West Ridge", "Uptown", "Lincoln Square", "Edison Park",
+                          "Norwood Park", "Jefferson Park", "Forest Glen", "North Park", "Albany Park",
+                          "O'Hare", "Edgewater", "North Center", "Lakeview", "Lincoln Park",
+                          "Avondale", "Logan Square", "Portage Park", "Irving Park", "Dunning",
+                          "Montclare", "Belmont Cragin", "Hermosa", "Near North Side", "Loop",
+                          "Near South Side", "Humboldt Park", "West Town", "Austin", "West Garfield Park",
+                          "East Garfield Park", "Near West Side", "North Lawndale", "South Lawndale", "Lower West Side",
+                          "Garfield Ridge", "Archer Heights", "Brighton Park", "McKinley Park", "New City",
+                          "West Elsdon", "Gage Park", "Clearing", "West Lawn", "Chicago Lawn",
+                          "West Englewood", "Englewood", "Armour Square", "Douglas", "Oakland",
+                          "Fuller Park", "Grand Boulevard", "Kenwood", "Washington Park", "Hyde Park",
+                          "Woodlawn", "South Shore", "Bridgeport", "Greater Grand Crossing", "Ashburn",
+                          "Auburn Gresham", "Beverly", "Washington Heights", "Mount Greenwood", "Morgan Park",
+                          "Chatham", "Avalon Park", "South Chicago", "Burnside", "Calumet Heights",
+                          "Roseland", "Pullman", "South Deering", "East Side", "West Pullman",
+                          "Riverdale", "Hegewisch"]
 
-    org_1 = Organization.create(name: "Organization 1", neighborhoods: [hood_1, hood_2])
-    org_2 = Organization.create(name: "Organization 2", neighborhoods: [hood_2, hood_3])
-    org_3 = Organization.create(name: "Organization 3", neighborhoods: [hood_3, hood_4])
-    org_4 = Organization.create(name: "Organization 4", neighborhoods: [hood_1, hood_4])
+    neighborhood_names.each do |name|
+      Neighborhood.create(name: name)
+    end
+
+    org_1 = Organization.create(name: "Organization 1", neighborhoods: [Neighborhood.first, Neighborhood.second])
+    org_2 = Organization.create(name: "Organization 2", neighborhoods: [Neighborhood.second, Neighborhood.third])
+    org_3 = Organization.create(name: "Organization 3", neighborhoods: [Neighborhood.third, Neighborhood.fourth])
+    org_4 = Organization.create(name: "Organization 4", neighborhoods: [Neighborhood.first, Neighborhood.fourth])
 
     user = User.create(email: "user@example.com",
                 first_name: "Jack",
                 last_name: "Reacher",
                 password: "user",
                 role: "registered_user",
-                neighborhood: hood_1)
+                neighborhood: Neighborhood.first)
     leader = User.create(email: "leader@example.com",
                 first_name: "John",
                 last_name: "Wick",
                 password: "leader",
                 role: "community_leader",
-                neighborhood: hood_1,
+                neighborhood: Neighborhood.first,
                 organizations: [org_1, org_2])
     admin = User.create(email: "admin@example.com",
                 first_name: "Jason",
                 last_name: "Bourne",
                 password: "admin",
                 role: "admin",
-                neighborhood: hood_1)
+                neighborhood: Neighborhood.first)
 
     Event.create(title: "Event 1",
                  host_contact: "person@example.com",
@@ -37,7 +53,7 @@ class Seed
                  date: Date.tomorrow + 10,
                  time: Time.now,
                  user: user,
-                 neighborhood: hood_1,
+                 neighborhood: Neighborhood.first,
                  organization: org_1)
     Event.create(title: "Event 2",
                  host_contact: "person@example.com",
@@ -46,7 +62,7 @@ class Seed
                  date: Date.tomorrow + 10,
                  time: Time.now + 20*60,
                  user: user,
-                 neighborhood: hood_1,
+                 neighborhood: Neighborhood.first,
                  organization: org_2)
     Event.create(title: "Event 3",
                  host_contact: "person@example.com",
@@ -55,7 +71,7 @@ class Seed
                  date: Date.tomorrow,
                  time: Time.now + 20*60*60,
                  user: user,
-                 neighborhood: hood_2,
+                 neighborhood: Neighborhood.second,
                  organization: org_2)
     Event.create(title: "Event 4",
                  host_contact: "person@example.com",
@@ -65,7 +81,7 @@ class Seed
                  time: Time.now,
                  status: "approved",
                  user: user,
-                 neighborhood: hood_2,
+                 neighborhood: Neighborhood.second,
                  organization: org_2)
 
     Artwork.create(title: "Art 1",
@@ -73,20 +89,20 @@ class Seed
                   description: "A description",
                   address: "5114 S. Elizabeth St., Chicago, IL 60609",
                   user: user,
-                  neighborhood: hood_1)
+                  neighborhood: Neighborhood.first)
     Artwork.create(title: "Art 2",
                    artist: "da Vinci",
                    description: "A description",
                    address: "5114 S. Elizabeth St., Chicago, IL 60609",
                    user: user,
-                   neighborhood: hood_2)
+                   neighborhood: Neighborhood.second)
     Artwork.create(title: "Art 3",
                    artist: "van Gogh",
                    description: "A description",
                    address: "5114 S. Elizabeth St., Chicago, IL 60609",
                    status: "approved",
                    user: user,
-                   neighborhood: hood_2)
+                   neighborhood: Neighborhood.second)
 
     Story.create(title: "Story 1",
                  author: "David Kelly",
@@ -94,14 +110,14 @@ class Seed
                  body: "A riveting story goes here",
                  address: "5114 S. Elizabeth St., Chicago, IL 60609",
                  user: user,
-                 neighborhood: hood_1)
+                 neighborhood: Neighborhood.first)
     Story.create(title: "Story 2",
                  author: "Mike Martinez",
                  description: "A description",
                  body: "A riveting story goes here",
                  address: "5114 S. Elizabeth St., Chicago, IL 60609",
                  user: user,
-                 neighborhood: hood_2)
+                 neighborhood: Neighborhood.second)
     Story.create(title: "Story 3",
                  author: "Some guy",
                  description: "A description",
@@ -109,7 +125,7 @@ class Seed
                  address: "5114 S. Elizabeth St., Chicago, IL 60609",
                  status: "approved",
                  user: user,
-                 neighborhood: hood_2)
+                 neighborhood: Neighborhood.second)
   end
 end
 
