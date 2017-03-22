361a0ecd3e1681c9fe23cd9467b504a50dcdb487
Zack Forbing <zackforbing@gmail.com>
2017-03-22 13:42:06 -0600

    fix merge conflicts

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,6 @@
 function createMap(){
   handler = Gmaps.build('Google');
+<<<<<<< Updated upstream
   handler.buildMap({provider: {
                                 disableDefaultUI: true,
                                 center: new google.maps.LatLng(41.8781136, -87.6297982),
