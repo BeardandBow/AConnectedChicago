a562e6129d872db6e87941d0d45672a700de4358
Zack Forbing <zackforbing@gmail.com>
2017-03-24 14:50:56 -0600

    implemented caching

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
-  timeout: 30                 # geocoding service timeout (secs)
-  # lookup: :google,            # name of geocoding service (symbol)
+  timeout: 30,              # geocoding service timeout (secs)
+  lookup: :google,            # name of geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   # api_key: nil,               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
+  cache: Rails.cache                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
