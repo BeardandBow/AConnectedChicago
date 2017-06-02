921ab822b902284c5b6759fc96dedeabde619aa6
Zack Forbing <zackforbing@gmail.com>
2017-06-01 19:04:51 -0600

    organization markers displaying correctly (#142)
    * organization markers displaying correctly

--- app/controllers/api/v1/neighborhoods_controller.rb
+++ app/controllers/api/v1/neighborhoods_controller.rb
@@ -6,7 +6,5 @@ class Api::V1::NeighborhoodsController < Api::V1::ApiBaseController
     @events = Event.where(status: "approved").where("date >= ?", Date.today).order(:date, :time).where(neighborhood: @hood)
     @stories = Story.where(status: "approved").where(neighborhood: @hood)
     @artworks = Artwork.where(status: "approved").where(neighborhood: @hood)
-    # response = {name: hood.name, bounds: hood.bounds, events: events, stories: stories, artworks: artworks}
-    # respond_with response
   end
 end
