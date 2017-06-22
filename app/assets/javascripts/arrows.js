2843aca2da9ef33dcc78f6b0d4c932d7ac87537f
Zack Forbing <zackforbing@gmail.com>
2017-06-22 15:10:45 -0600

    lunch commit - events formatter completed, todo: finish delete button, format stories, animations

--- app/assets/javascripts/arrows.js
+++ app/assets/javascripts/arrows.js
@@ -1,22 +1,15 @@
 var currentIndex
+var currentUser = findCurrentUser();
 $(document).ready(function(){
   var hoodName = document.getElementById('left-arrow').firstChild.getAttribute("data-neighborhood");
   var submissionType = document.getElementById('left-arrow').firstChild.getAttribute("data-type");
-  var currentUser = findCurrentUser();
   findAllSubmissions(hoodName, submissionType);
 });
 
-function showPrevious (previous, submissions, submissionType, hoodName){
-  currentIndex = submissions.indexOf(previous);
+function incrementSubmission (submission, submissions, submissionType, hoodName){
+  currentIndex = submissions.indexOf(submission);
   console.log(currentIndex)
-  buildNewShowPage(submissionType, previous);
-  findAllSubmissions(hoodName, submissionType);
-};
-
-function showNext(next, submissions, submissionType, hoodName){
-  currentIndex = submissions.indexOf(next);
-  console.log(currentIndex)
-  buildNewShowPage(submissionType, next);
+  buildNewShowPage(submissionType, submission);
   findAllSubmissions(hoodName, submissionType);
 };
 
@@ -34,22 +27,27 @@ function findAllSubmissions(hoodName, submissionType) {
       if (currentIndex + 1 === submissions.length) {
         next = submissions[0]
       } else {
-        next = submissions[++currentIndex]
+        next = submissions[currentIndex + 1]
       }
-
       if (currentIndex - 1 < 0) {
-        previous = submissions[-1]
+        previous = submissions[submissions.length - 1]
       } else {
-        previous = submissions[--currentIndex]
+        previous = submissions[currentIndex - 1]
       }
     })
     var leftArrow = document.getElementById('left-arrow');
     var rightArrow = document.getElementById('right-arrow');
-    leftArrow.addEventListener('click', function(){
-      showPrevious(previous, submissions, submissionType, hoodName);
+    if (typeof previousSubmission != "undefined") {
+      leftArrow.removeEventListener('click', previousSubmission)
+    }
+    leftArrow.addEventListener('click', previousSubmission = function(){
+      incrementSubmission(previous, submissions, submissionType, hoodName);
     });
-    rightArrow.addEventListener('click', function(){
-      showNext(next, submissions, submissionType, hoodName);
+    if (typeof nextSubmission != "undefined") {
+      rightArrow.removeEventListener('click', nextSubmission)
+    }
+    rightArrow.addEventListener('click', nextSubmission = function(){
+      incrementSubmission(next, submissions, submissionType, hoodName);
     });
   });
 }
@@ -73,16 +71,16 @@ function formatArtworkShow(artwork) {
   var address = document.getElementById("address");
   var formattedDateTime = document.getElementById("datetime");
   var deleteDiv = document.getElementById("delete");
+  clearDiv(image);
+  clearDiv(deleteDiv);
   title.innerHTML = artwork.title;
   description.innerHTML = artwork.description;
   artist.innerHTML = "By " + artwork.artist;
   address.innerHTML = artwork.address;
   formattedDateTime.innerHTML = "Posted On " + artwork.formatted_create_time;
-  clearDiv(image);
-  clearDiv(deleteDiv);
-  if (artwork.image.url) {
+  if (artwork.image_url) {
     var imgTag = document.createElement("img");
-    imgTag.src = artwork.image.url;
+    imgTag.src = artwork.image_url;
     imgTag.alt = "artwork image";
     image.appendChild(imgTag);
   }
@@ -95,27 +93,65 @@ function formatArtworkShow(artwork) {
 function formatEventShow(event) {
   var title = document.getElementById("title");
   var image = document.getElementById("image");
-  var dateTime = document.createElement("p");
-  var description = document.createElement("p");
-  var event_type = document.createElement("p");
-  description.innerHTML = event.description
-  dateTime.innerHTML = event.formatted_date_time
-  event_type.innerHTML = event.type
-}
-
-function formatStoryShow(story) {
-  var title = document.getElementById("title");
-  var author = document.createElement("p");
-  var description = document.createElement("p");
-  var image = document.createElement("img");
-  heading.innerHTML = story.title.link("/stories/" + story.id);
-  description.innerHTML = stringTruncate(story.description, 50);
-  author.innerHTML = "by " + story.author
-  listing.appendChild(heading);
-  if (story.image.thumb.url) {
-    image.src = story.image.thumb.url;
+  var organization = document.getElementById("organization");
+  var dateTime = document.getElementById("formatted-date-time");
+  var mailTo = document.getElementById("mail-to");
+  var address = document.getElementById("address");
+  var eventType = document.getElementById("type");
+  var description = document.getElementById("description");
+  var linkDiv = document.getElementById("link");
+  var deleteDiv = document.getElementById("delete");
+  clearDiv(image);
+  clearDiv(mailTo);
+  clearDiv(linkDiv);
+  clearDiv(deleteDiv);
+  title.innerHTML = event.title;
+  if (event.organization.website) {
+    organization.innerHTML = "Hosted By ";
+    var orgLink = document.createElement("a");
+    orgLink.href = event.organization.website;
+    orgLink.innerHTML = event.organization.name;
+    organization.appendChild(orgLink);
+  } else {
+    organization.innerHTML = "Hosted By " + event.organization.name
+  }
+  dateTime.innerHTML = event.formatted_date_time;
+  var mailToLink = document.createElement("a")
+  mailToLink.href = "mailto:" + event.host_contact
+  mailToLink.innerHTML = "Contact for more information"
+  mailTo.appendChild(mailToLink)
+  address.innerHTML = event.address;
+  eventType.innerHTML = event.type;
+  description.innerHTML = event.description;
+  var linkDivLink = document.createElement("a")
+  linkDivLink.href = event.link
+  linkDivLink.innerHTML = "View Event Page"
+  linkDiv.appendChild(linkDivLink)
+  if (event.image_url) {
+    var imgTag = document.createElement("img");
+    imgTag.src = event.image_url;
+    imgTag.alt = "event image";
+    image.appendChild(imgTag);
+  }
+  if (isDeletable(event, currentUser)) {
+    var deleteButton = document.createElement("button")
+    deleteButton.addEventListener()
   }
 }
+
+// function formatStoryShow(story) {
+//   var title = document.getElementById("title");
+//   var author = document.createElement("p");
+//   var description = document.createElement("p");
+//   var image = document.createElement("img");
+//   heading.innerHTML = story.title.link("/stories/" + story.id);
+//   description.innerHTML = stringTruncate(story.description, 50);
+//   author.innerHTML = "by " + story.author
+//   listing.appendChild(heading);
+//   if (story.image.thumb.url) {
+//     image.src = story.image.thumb.url;
+//   }
+// }
 
 function findCurrentUser() {
   $.get("/api/v1/users/", function(response) {
@@ -127,11 +163,11 @@ function isDeletable(submission, currentUser) {
   if (currentUser) {
     if (submission.user_id == currentUser.id) {
       return true;
+    } else if (currentUser.role == "admin") {
+      return true;
     } else if (currentUser.role == "community_leader" && currentUser.organizations.some(function(e) {e.name === submission.organization.name}).length > 0) {
       return true;
     } else if (currentUser.role == "community_leader" && currentUser.neighborhood_id == submission.neighborhood_id) {
-      return true;
-    } else if (currentUser.role == "admin") {
       return true;
     } else {
       return false;
--- app/views/api/v1/neighborhoods/show.json.jbuilder
+++ app/views/api/v1/neighborhoods/show.json.jbuilder
@@ -11,42 +11,49 @@ json.locations @hood.locations.includes(:organization) do |location|
   end
 end
 json.events @events do |event|
-  json.(event, :id,
-              :title,
-              :host_contact,
-              :description,
-              :map_lat,
-              :map_long,
-              :formatted_date_time,
-              :image,
-              :address,
-              :status,
-              :pkey)
+  json.id event.id
+  json.title event.title
+  json.host_contact event.host_contact
+  json.description event.description
+  json.map_lat event.map_lat
+  json.map_long event.map_long
+  json.formatted_date_time event.formatted_date_time
+  json.image_url event.image.url
+  json.thumb_url event.image.thumb.url
+  json.address event.address
+  json.status event.status
+  json.pkey event.pkey
   json.type event.type.name
+  json.organization do
+    json.name event.organization.name
+    json.website event.organization.website
+  end
 end
 json.artworks @artworks do |artwork|
-  json.(artwork, :id,
-                 :title,
-                 :artist,
-                 :description,
-                 :map_lat,
-                 :map_long,
-                 :formatted_create_time,
-                 :image,
-                 :address,
-                 :status,
-                 :pkey)
+  json.id artwork.id
+  json.title artwork.title
+  json.artist artwork.artist
+  json.description artwork.description
+  json.map_lat artwork.map_lat
+  json.map_long artwork.map_long
+  json.formatted_create_time artwork.formatted_create_time
+  json.image_url artwork.image.url
+  json.thumb_url artwork.image.thumb.url
+  json.address artwork.address
+  json.status artwork.status
+  json.pkey artwork.pkey
 end
 json.stories @stories do |story|
-  json.(story, :id,
-               :title,
-               :author,
-               :description,
-               :map_lat,
-               :map_long,
-               :formatted_create_time,
-               :image,
-               :address,
-               :status,
-               :pkey)
+  json.id story.id
+  json.title story.title
+  json.author story.author
+  json.description story.description
+  json.map_lat story.map_lat
+  json.map_long story.map_long
+  json.formatted_create_time story.formatted_create_time
+  json.image_url story.image.url
+  json.thumb_url story.image.thumb.url
+  json.address story.address
+  json.status story.status
+  json.pkey story.pkey
 end
