bb77fef830f1d6fe2a6753ffa1c9e9535c426f80
Caleb <calebcowen@gmail.com>
2017-11-06 20:26:21 -0600

    Try to implement double tap

--- public/assets/application-0a929f3e5a0777d7f33e1c1760e02dbc03279163f91f47a4019ca0cb2d80b093.js
+++ public/assets/application-0a929f3e5a0777d7f33e1c1760e02dbc03279163f91f47a4019ca0cb2d80b093.js
@@ -18423,13 +18423,19 @@ var hoodStyle = [
 ]
 ;
 $("#modal").ready(function(){
+  var touches = 0
   var currentDate = new Date().toDateString()
   if (localStorage.getItem("lastVisited")) {
     checkDate(currentDate)
   } else {
     var body = document.querySelector("body")
     body.addEventListener('click', hideModal, false)
-    body.addEventListener('touchend', hideModal, false)
+    body.addEventListener('touchend', function() {
+      touches += 1
+      if (touches >= 2) {
+        hideModal()
+      }
+    })
 
     localStorage.setItem("lastVisited", currentDate)
     $("#navbar").hide();
